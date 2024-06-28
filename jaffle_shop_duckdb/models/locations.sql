c63d3ac6565ec2939696dab53df74eeb129db648
Sean Lopp <lopp@elementl.com>
2024-06-28 10:16:10 -0600

    update dbt dep name

--- jaffle_shop_duckdb/models/locations.sql
+++ jaffle_shop_duckdb/models/locations.sql
@@ -1,4 +1,4 @@
 SELECT * 
 FROM {{ ref("stg_customers") }}  AS customers
-LEFT JOIN {{ source("main","raw_locations") }} AS locations 
+LEFT JOIN {{ source("main","locations") }} AS locations 
 ON locations.last_name = customers.last_name
