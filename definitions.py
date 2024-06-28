3d28d95fa7109640b91e5ecdb93e7208b1e1744f
Sean Lopp <lopp@elementl.com>
2024-06-28 10:29:08 -0600

    debugging

--- definitions.py
+++ definitions.py
@@ -1,22 +1,24 @@
 import json
 from pathlib import Path
 
-from dagster import Definitions, external_assets_from_specs, AssetSpec
+from dagster import Definitions, external_assets_from_specs, AssetSpec, AssetKey
 from dagster_dbt import build_dbt_asset_specs
 
 # create an external asset for one of the dbt sources
 # see https://docs.dagster.io/concepts/assets/external-assets
 
-source_assets = external_assets_from_specs([
-    AssetSpec(
-        key="locations",
-        description="locations table created by some external job that loads raw locations data",
-        tags={"dagster/storage_kind": "duckdb"}
-    )
-])
+source_assets = external_assets_from_specs(
+    [
+        AssetSpec(
+            key=AssetKey(["main", "raw_locations"]),
+            description="locations table created by some external job that loads raw locations data",
+            tags={"dagster/storage_kind": "duckdb"},
+        )
+    ]
+)
 
 # load external assets from dbt manifest
-project_dir = Path(__file__).joinpath("..","jaffle_shop_duckdb").resolve()
+project_dir = Path(__file__).joinpath("..", "jaffle_shop_duckdb").resolve()
 manifest = json.loads(project_dir.joinpath("target", "manifest.json").read_bytes())
 
 jaffle_shop_dbt_assets = external_assets_from_specs(
--- run_dbt.py
+++ run_dbt.py
@@ -1,42 +1,40 @@
 import os
 from pathlib import Path
-import duckdb 
+import duckdb
 import httpx
 from dbt.cli.main import dbtRunner, dbtRunnerResult
 
 # see https://docs.dagster.io/apidocs/external-assets-rest
 REPORT_ASSET_MATERIALIZATION_URL = "http://localhost:3000/report_asset_materialization/"
 
+
 def report_dbt_metadata_to_dagster(dbt_model_asset_key, metadata):
-    """ report metadata to dagster about externally managed assets """
+    """report metadata to dagster about externally managed assets"""
     httpx.post(
-                url=REPORT_ASSET_MATERIALIZATION_URL,
-                json={
-                    "asset_key": dbt_model_asset_key,
-                    "metadata": metadata
-                }
-            )
-    return 
+        url=REPORT_ASSET_MATERIALIZATION_URL,
+        json={"asset_key": dbt_model_asset_key, "metadata": metadata},
+    )
+    return
 
 
 def create_duckdb_locations_table():
-    """ Create a table that will be used as a dbt source """
-    con = duckdb.connect(database="./jaffle_shop_duckdb/jaffle_shop.duckdb", read_only=False)
+    """Create a table that will be used as a dbt source"""
+    con = duckdb.connect(database="jaffle_shop.duckdb", read_only=False)
     con.execute(
         """ 
-            CREATE TABLE locations AS 
-                SELECT * FROM read_csv("./jaffle_shope_duckdb/seeds/raw_customeres.csv")
+            CREATE OR REPLACE TABLE raw_locations AS 
+                SELECT * FROM read_csv("./jaffle_shop_duckdb/seeds/raw_customers.csv")
         
         """
     )
 
-    report_dbt_metadata_to_dagster("locations", {})
+    report_dbt_metadata_to_dagster(["main", "raw_locations"], {})
+
 
 def run_dbt_and_report_metadata():
-    """ Run dbt project and report the results to dagster """
+    """Run dbt project and report the results to dagster"""
 
     project_dir = Path(__file__).joinpath("..", "jaffle_shop_duckdb").resolve()
-    
 
     # run dbt however you want, here based on https://docs.getdbt.com/reference/programmatic-invocations
     dbt = dbtRunner()
@@ -45,16 +43,17 @@ def run_dbt_and_report_metadata():
 
     if res.exception is None:
         for result in res.result.results:
-            if result.node.resource_type in ['model', 'seed']:
+            if result.node.resource_type in ["model", "seed"]:
                 name = result.node.identifier
                 execution_time = result.execution_time
                 column_names = list(result.node.columns.keys())
                 metadata = {
                     "execution_time": execution_time,
-                    "column_names": column_names
+                    "column_names": column_names,
                 }
 
                 report_dbt_metadata_to_dagster(name, metadata)
+
 
 if __name__ == "__main__":
     create_duckdb_locations_table()
